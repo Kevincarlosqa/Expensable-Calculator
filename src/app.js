b76b36e31c1e9b9983b958e50debf3a7a9b7ccd2
Christian Fita <chrisfitaram@gmail.com>
2023-06-16 23:37:51 +0200

    ADD CONTAINER-KEYBOARD

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import Container from "./components/container";
 import CalendarDate from "./components/date";
 import Display from "./components/display";
 import Keyboard from "./components/keyboard";
+import ContainerKeyboard from "./components/container-keyboard";
 
 function App() {
   return (
     <Container>
       <Header>Hello</Header>
       <Display>fdsa</Display>
-      <Keyboard></Keyboard>
+      <ContainerKeyboard><Keyboard></Keyboard><Keyboard></Keyboard><Keyboard></Keyboard><Keyboard></Keyboard><Keyboard></Keyboard><Keyboard></Keyboard><Keyboard></Keyboard><Keyboard></Keyboard></ContainerKeyboard>
       <CalendarDate>wewe</CalendarDate>
     </Container>
   );
