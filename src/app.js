238fddf03a64f02792424cf5bc089b669126b492
Kevin Quispe <kevincarlosqa@gmail.com>
2023-06-16 14:53:38 -0500

    add: Init React

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import Header from "./components/header";
+import Container from "./components/container";
+
+function App() {
+  return (
+    <Container>
+      <Header>Hello</Header>
+    </Container>
+  );
+}
+
+export default App;
